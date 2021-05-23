c66ede6f54f1478ae5c116ec53b44d93d96ba30c
soorajshankar <soorajshankar@gmail.com>
2021-05-23 15:32:20 +0530

    UI tabs

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -3,10 +3,38 @@ import SqlExecuter from './SqlExecuter';
 
 // TODO move session to context/env for better access
 // select screen based on the sidebar selection
-const MainScreen = ({ session = '' }) => {
-  const [selectedTab, setSelectedTab] = useState();
+const MainScreen: React.FC<{ session: string }> = ({ session = '' }) => {
+  const [selectedTab, setSelectedTab] = useState('SQL');
 
-  return <SqlExecuter session={session} />;
+  return (
+    <div className="w-screen h-screen flex text-gray-800">
+      <div className="bg-gray-800 text-gray-100 w-1/12 pt-8 flex flex-col ">
+        <button type="button">
+          <span role="img" aria-label="app-icon" className="pl-6">
+            ⚡️
+          </span>
+        </button>
+        <button
+          type="button"
+          className={`h-20 ${selectedTab === 'SQL' && 'bg-blue-200'}`}
+          onClick={() => setSelectedTab('SQL')}
+        >
+          SQL{' '}
+        </button>
+        <button
+          type="button"
+          className={`h-20 text-xs ${selectedTab === 'TABLE' && 'bg-blue-200'}`}
+          onClick={() => setSelectedTab('TABLE')}
+        >
+          Tables/Views
+        </button>
+      </div>
+      <div className="bg-gray-100 w-11/12 flex flex-row">
+        {selectedTab === 'SQL' && <SqlExecuter session={session} />}
+        {selectedTab === 'TABLE' && <div>Test</div>}
+      </div>
+    </div>
+  );
 };
 
 export default MainScreen;
