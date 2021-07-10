bed20c975555d5e25add6e084df2bea2fadabda1
soorajshankar <soorajshankar@gmail.com>
2021-07-10 17:46:05 +0530

    disk usage barchart with data

--- src/components/Overview/DiskUsageSection.tsx
+++ src/components/Overview/DiskUsageSection.tsx
@@ -1,20 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { ReactText, useEffect, useState } from 'react';
-import {
-  BarChart,
-  Bar,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  PieChart,
-  Pie,
-} from 'recharts';
+
 import { QueryResultRow } from 'pg';
 
 import TotalTableSize from './TotalTableSize';
 import TableSize from './TableSize';
 import TotalIndexSize from './TotalIndexSize';
 import DbSession, { SqlExecReponseType } from '../../sessions/DbSession';
-import { DiskUsageStateType } from './types';
+import { CustomTooltipProps, DiskUsageStateType } from './types';
+import PieChart from './PieChart';
+import BarChart from './BarChart';
 
 const totalTableSize = `SELECT c.relname AS name,
 pg_size_pretty(pg_total_relation_size(c.oid)) AS size
@@ -46,133 +41,25 @@ GROUP BY c.relname
 ORDER BY sum(c.relpages) DESC;
   `;
 
-const data = [
-  {
-    name: 'Page A',
-    table: 4000,
-    index: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    table: 3000,
-    index: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    table: 2000,
-    index: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    table: 2780,
-    index: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    table: 1890,
-    index: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page A',
-    table: 4000,
-    index: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    table: 3000,
-    index: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    table: 2000,
-    index: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    table: 2780,
-    index: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    table: 1890,
-    index: 4800,
-    amt: 2181,
-  },
-];
+const getBarChartData = (state: DiskUsageStateType) => {
+  const resultMap = {} as Record<string, any>;
+  state?.tableData?.rows?.forEach(({ name, size }: any) => {
+    resultMap[name] = {
+      tableSize: Number.parseInt(size, 10),
+    };
+  });
+  state?.totalTableData?.rows?.forEach(({ name, size }: any) => {
+    resultMap[name] = {
+      ...(resultMap[name] && resultMap[name]),
+      totalTableSize: Number.parseInt(size, 10),
+    };
+  });
+  const result = Object.entries(resultMap).map(([key, value]) => ({
+    name: key,
+    ...value,
+  }));
 
-type CustomTooltipProps = {
-  active: boolean;
-  payload: {
-    name: ReactText;
-    value: ReactText;
-  }[];
-};
-const CustomTooltip = ({ active, payload }: CustomTooltipProps) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className="custom-tooltip bg-gray-800 border-gray-300 p-2">
-        <p className="label">{`${payload?.[0]?.name} : ${payload?.[0]?.value} kB`}</p>
-      </div>
-    );
-  }
-
-  return null;
-};
-
-const ChartsSection = () => (
-  <div className="w-full" style={{ height: '33vh' }}>
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="table" stackId="a" fill="#8884d8" />
-        <Bar dataKey="index" stackId="a" fill="#82ca9d" />
-      </BarChart>
-    </ResponsiveContainer>
-  </div>
-);
-
-type PieDataType = {
-  name: ReactText;
-  value: ReactText;
-};
-
-const PieChartsSection = ({ pieData = [] }: { pieData: PieDataType[] }) => {
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart>
-        <Pie
-          dataKey="value"
-          isAnimationActive={false}
-          data={pieData}
-          cx={170}
-          cy={170}
-          outerRadius={80}
-          fill="#8884d8"
-          label
-        />
-
-        <Tooltip content={<CustomTooltip />} />
-      </PieChart>
-    </ResponsiveContainer>
-  );
+  return result;
 };
 
 const DiskUsageSection = ({ session }: { session: DbSession }) => {
@@ -198,28 +85,29 @@ const DiskUsageSection = ({ session }: { session: DbSession }) => {
     getdata();
   }, []);
   const pieData =
-    state?.totalTableData?.rows?.map(({ name, size }) => ({
+    state?.indexData?.rows?.map(({ name, size }) => ({
       name,
       value: Number.parseInt(size, 10),
     })) ?? [];
+  const barChartData = getBarChartData(state);
 
   return (
     <>
       <h1 className="text-2xl p-4">Disk Usage</h1>
       <div className="w-full rounded-xl bg-gray-800">
         <div className="p-4">
-          <div className="flex m-auto">
-            <div className="flex-1">
-              <PieChartsSection pieData={pieData} />
+          <div className="grid grid-cols-3 gap-2">
+            <div className="col-span-2">
+              <BarChart barchartData={barChartData} />
             </div>
-            <div className="flex-1">
-              <ChartsSection />
+            <div className="col">
+              <PieChart pieData={pieData} />
             </div>
             {/* <ChartsSection /> */}
           </div>
           <div className="w-full flex">
-            <TotalTableSize data={state?.totalTableData} />
             <TableSize data={state?.tableData} />
+            <TotalTableSize data={state?.totalTableData} />
             <TotalIndexSize data={state?.indexData} />
           </div>
         </div>
--- src/components/Overview/types.ts
+++ src/components/Overview/types.ts
@@ -1,8 +1,17 @@
 import { QueryResultRow } from 'pg';
+import { ReactText } from 'react';
 import { SqlExecReponseType } from '../../sessions/DbSession';
 
 export type DiskUsageStateType = {
   totalTableData?: SqlExecReponseType<QueryResultRow[]>;
   tableData?: SqlExecReponseType<QueryResultRow[]>;
   indexData?: SqlExecReponseType<QueryResultRow[]>;
+};
+
+export type CustomTooltipProps = {
+  active: boolean;
+  payload: {
+    name: ReactText;
+    value: ReactText;
+  }[];
 };
