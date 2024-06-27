3503c13c663d897a16173be60ab825ff8743831f
Sooraj Sanker <soorajshankar@gmail.com>
2024-06-27 13:45:16 +0530

    Install PG extensions

--- web/app/overview/Overview/InstalledExtensions.tsx
+++ web/app/overview/Overview/InstalledExtensions.tsx
@@ -2,26 +2,29 @@
 import React, { useEffect, useState } from "react";
 import DbSession from "../../sessions/DbSession";
 import ReactTooltip from "react-tooltip";
+import { useRouter } from "next/navigation";
 
 const extensionsQuery = `SELECT * FROM pg_available_extensions;`;
 
-interface ExtensioList {
+interface ExtensionList {
+  name: string;
+  default_version: string;
+  installed_version: string;
   comment: string;
-  value: string;
-  extension: string;
 }
 
 const InstalledExtensions = ({ session }: { session: DbSession }) => {
   const [indexUsage, setInstalledExtensions] = useState<{
     status: string;
-    rows: ExtensioList[];
+    rows: ExtensionList[];
   }>({} as any);
+  const router = useRouter();
+
   useEffect(() => {
     const getData = async () => {
       try {
         const response = (await session.executeSQL(extensionsQuery)) as any;
-        console.log(response);
-        const res: ExtensioList[] =
+        const res: ExtensionList[] =
           response?.rows?.map(
             (i: {
               name: any;
@@ -29,11 +32,10 @@ const InstalledExtensions = ({ session }: { session: DbSession }) => {
               default_version: any;
               installed_version: any;
             }) => ({
-              extension: i.name,
+              name: i.name,
               comment: i.comment,
-              value: `${i.installed_version ?? "not installed"} / ${
-                i.default_version
-              }`,
+              default_version: i.default_version,
+              installed_version: i.installed_version,
             })
           ) ?? [];
 
@@ -44,42 +46,92 @@ const InstalledExtensions = ({ session }: { session: DbSession }) => {
     };
     getData();
   }, []);
+
+  const onInstallOrUninstall = (
+    extension: string,
+    installed_version?: string
+  ) => {
+    if (!installed_version) {
+      // ask user to install the extension
+      const userConfirmed = confirm(
+        `Do you want to install the extension ${extension}? This will open SQL editor with the installation query, you can then execute the query manually.`
+      );
+      if (!userConfirmed) {
+        return;
+      }
+      router.push(`/sql?sql=CREATE EXTENSION IF NOT EXISTS ${extension};`);
+    } else {
+      // ask user to uninstall the extension
+      const userConfirmed = confirm(
+        `Do you want to uninstall the extension ${extension}? This will open SQL editor with the uninstallation query, you can then execute the query manually.`
+      );
+      if (!userConfirmed) {
+        return;
+      }
+      router.push(`/sql?sql=DROP EXTENSION IF EXISTS ${extension};`);
+    }
+  };
+
   return (
     <div className="flex-1 rounded-xl bg-gray-800 overflow-auto p-2 m-2 shadow-lg font-semibold">
       <span className="flex justify-between w-full">
         <h1 className="text-xl">Extensions</h1>
         <h1 className="font-thin">installed / available</h1>
       </span>
-      <ExtensioList list={indexUsage?.rows ?? []} />
+      <ExtensionList
+        list={indexUsage?.rows ?? []}
+        onInstallOrUninstall={onInstallOrUninstall}
+      />
     </div>
   );
 };
 
-const ExtensioList = ({ list = [] }: { list: ExtensioList[] }) => {
+const ExtensionList = ({
+  list = [],
+  onInstallOrUninstall,
+}: {
+  list: ExtensionList[];
+  onInstallOrUninstall: (extension: string, installed_version?: string) => void;
+}) => {
   return (
     <ul className="py-2 text-sm overflow-auto" style={{ height: "10rem" }}>
-      {list.map(({ comment, value, extension }) => (
-        <li
-          key={extension}
-          className="flex w-full text-sm leading-normal hover:bg-gray-600 rounded hover:shadow-lg hover:drop-shadow hover:text-gray-100 px-1"
-        >
-          <span className="flex-1">
-            {extension}<span className="font-thin">{` : ${comment}`}</span>
-          </span>
-          <span className="text-green-50">{value}</span>
-          {/* <button
-            className="p-0 font-mono hover:text-green-500"
-            data-tip
-            data-for={`install-extension-${extension}`}
-          >↓
-          </button>
+      {list.map(({ comment, default_version, installed_version, name }) => (
+        <>
+          <li
+            key={name}
+            className="flex w-full text-sm leading-normal hover:bg-gray-600 rounded hover:shadow-lg hover:drop-shadow hover:text-gray-100 px-1"
+          >
+            {/* <button
+              className="px-2 font-mono hover:text-green-500"
+              // className=" hover:font-bold text-white p-0 font-thin font-mono rounded"
+            >
+              ⬇️
+            </button> */}
 
-          <ReactTooltip id={`install-extension-${extension}`} type="info">
-            <span>
-              Install the extension {extension} on the database. <br />
+            <span className="flex-1">
+              {name}
+              <span className="font-thin">{` : ${comment}`}</span>
             </span>
-          </ReactTooltip> */}
-        </li>
+
+            <a
+              className="text-green-50 hover:cursor-pointer"
+              data-tip
+              data-for={`install-extension-${name}`}
+              onClick={() => onInstallOrUninstall(name, installed_version)}
+            >{`${
+              installed_version ?? "not installed"
+            } / ${default_version}`}</a>
+          </li>
+          <ReactTooltip id={`install-extension-${name}`} type="info">
+            <span>
+              Click to {installed_version ? "unintall" : "install"} the
+              extension {name} on the connected database,
+              <br /> This will open SQL editor with the{" "}
+              {installed_version ? "unintallation" : "installation"} query, you
+              can then execute the query manually. <br />
+            </span>
+          </ReactTooltip>
+        </>
       ))}
       {list?.length === 0 && (
         <li className="flex w-full text-sm leading-normal">
--- web/app/sql/SqlScreen.tsx
+++ web/app/sql/SqlScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import SqlExecutor from "./SqlExecutor";
 import SqlHistory from "./SqlHistory";
 import SideHeader from "../components/atoms/SideHeader";
@@ -6,19 +7,31 @@ import PgSession from "../sessions/PgSession";
 import Tabs from "./Tabs";
 
 const SqlScreen = () => {
-  const initialTabs = typeof window !== "undefined" && localStorage.getItem("sql_tabs")
-    ? JSON.parse(localStorage.getItem("sql_tabs") as string)
-    : [{ id: 1, title: "Query 1" }];
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  const initialActiveTabId = typeof window !== "undefined" && parseInt(localStorage.getItem("sql_activeTabId") as string) || 1;
-  const initialSelectedSql = typeof window !== "undefined" && localStorage.getItem(`sql_${initialActiveTabId}`) || "SELECT now();";
+  const initialTabs =
+    typeof window !== "undefined" && localStorage.getItem("sql_tabs")
+      ? JSON.parse(localStorage.getItem("sql_tabs") as string)
+      : [{ id: 1, title: "Query 1" }];
+
+  const initialActiveTabId =
+    (typeof window !== "undefined" &&
+      parseInt(localStorage.getItem("sql_activeTabId") as string)) ||
+    1;
+  const initialSelectedSql =
+    (typeof window !== "undefined" &&
+      localStorage.getItem(`sql_${initialActiveTabId}`)) ||
+    "SELECT now();";
 
   const [tabs, setTabs] = useState(initialTabs);
   const [activeTabId, setActiveTabId] = useState(initialActiveTabId);
   const [selectedSql, setSelectedSql] = useState<string>(initialSelectedSql);
   const session = new PgSession("PG");
 
-  const [maxTabId, setMaxTabId] = useState(initialTabs.length > 0 ? Math.max(...initialTabs.map(tab => tab.id)) : 1);
+  const [maxTabId, setMaxTabId] = useState(
+    initialTabs.length > 0 ? Math.max(...initialTabs.map((tab) => tab.id)) : 1
+  );
 
   const addTab = () => {
     const newTabId = maxTabId + 1;
@@ -46,7 +59,9 @@ const SqlScreen = () => {
         setActiveTabId(newTabs[0].id);
         if (typeof window !== "undefined") {
           localStorage.setItem("sql_activeTabId", newTabs[0].id.toString());
-          setSelectedSql(localStorage.getItem(`sql_${newTabs[0].id}`) || "SELECT now();");
+          setSelectedSql(
+            localStorage.getItem(`sql_${newTabs[0].id}`) || "SELECT now();"
+          );
         }
       } else {
         addTab();
@@ -56,15 +71,28 @@ const SqlScreen = () => {
 
   useEffect(() => {
     // Ensure the activeTabId is within the current tabs array
-    if (!tabs.some(tab => tab.id === activeTabId)) {
+    if (!tabs.some((tab) => tab.id === activeTabId)) {
       setActiveTabId(tabs.length > 0 ? tabs[0].id : 1);
     }
     if (typeof window !== "undefined") {
       localStorage.setItem("sql_tabs", JSON.stringify(tabs));
       localStorage.setItem("sql_activeTabId", activeTabId.toString());
-      setSelectedSql(localStorage.getItem(`sql_${activeTabId}`) || "SELECT now();");
+      setSelectedSql(
+        localStorage.getItem(`sql_${activeTabId}`) || "SELECT now();"
+      );
     }
   }, [tabs, activeTabId]);
+
+  useEffect(() => {
+    const sqlQuery = searchParams?.get("sql");
+    if (sqlQuery) {
+      setSelectedSql(decodeURIComponent(sqlQuery));
+      // Remove the query parameter from the URL
+      const currentUrl = new URL(window.location.href);
+      currentUrl.searchParams.delete("sql");
+      router.replace(currentUrl.toString());
+    }
+  }, [searchParams, router]);
 
   return (
     <div className="flex w-full h-full text-gray-200">
