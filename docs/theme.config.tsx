a186aba2ebbb9f9d6d663095e5f473540e2b0ee4
Sooraj Sanker <soorajshankar@gmail.com>
2024-07-09 18:58:45 +0530

    add seo tags

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -9,6 +9,39 @@ const config: DocsThemeConfig = {
   chat: {
     link: "https://discord.gg/n6fNgRdX",
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s | DB Lens",
+      twitter: {
+        handle: "@db_lens",
+      },
+      openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://dblens.app/',
+        site_name: 'DB Lens',
+        images: [
+          {
+            url: 'https://dblens.app/images/favicon.png',
+            width: 1200,
+            height: 630,
+            alt: 'DB Lens',
+          },
+        ],
+      },
+      additionalMetaTags: [
+        {
+          property: "author",
+          content: "Sooraj Sanker",
+        },
+        {
+          name: "keywords",
+          content:
+            "DB Lens, database exploration, database analysis, SQL, ER diagrams",
+        },
+      ],
+    };
+  },
   docsRepositoryBase: "https://github.com/dblens/app",
   footer: {
     text: "DB Lens © 2024",
